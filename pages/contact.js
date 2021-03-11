574715c35a87eb626be0d7e9f6174e1050fe7576
ethicalhub <yasiransari.elith@gmail.com>
2021-03-12 00:17:48 +0530

    Update Contact Page

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import axios from 'axios'
 import Head from "next/head";
 import Footer from '../components/footer'
 import Nav from "../components/nav";
@@ -5,13 +7,39 @@ import contactStyles from '../styles/contact.module.scss'
 
 const contact = () => {
 
+    const [formData, setFormData] = useState({
+        fullName:'',
+        email: '',
+        phone: '',
+        message: ''
+    })
+
+    const handleChange = (event)=>{
+        const {name, value} = event.target
+
+        setFormData((prevs)=>{
+            return{
+                ...prevs,
+                [name] : value
+            }
+        })
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+        alert(`${ formData.fullName} , ${formData.email} , ${formData.phone} , ${formData.message}`)
+
+        setFormData({
+            fullName:'',
+            email: '',
+            phone:'',
+            message:''
+        })
+    }
 
 
 
-
-
-
-    
     return (
         <>
             <Head>
@@ -30,43 +58,46 @@ const contact = () => {
             <div className = {contactStyles.container}>
                 <p className={contactStyles.para}>We’d love to discuss how we can help you.</p>
                 <form
-            // onSubmit={handleSubmit} 
-            className = {contactStyles.contactForm}
-            >
-                <input 
+                onSubmit={handleSubmit} 
+                className = {contactStyles.contactForm}
+                >
+                    <input 
+                        required
+                        type='text'
+                        placeholder='Your Name'
+                        name='fullName'
+                        onChange={handleChange}
+                        value={formData.fullName}
+                    />
+
+                    <input
+                        required
+                        type='email'
+                        placeholder='Enter Your Email'
+                        name='email'
+                        onChange={handleChange}
+                        value={formData.email}
+                    />
+                
+                    <input
+                        required
+                        type='number'
+                        placeholder='Enter Your Phone Number'
+                        name='phone'
+                        onChange={handleChange}
+                        value={formData.phone}
+                    />
+
+                    <textarea 
                     required
                     type='text'
-                    placeholder='Your Name'
-                    name='fullName'
-                    // onChange={handleChange}
-                    // value={userData.fullName}
-                />
-                <input
-                    required
-                    type='email'
-                    placeholder='Enter Your Email'
-                    name='email'
-                    // onChange={handleChange}
-                    // value={userData.email}
-                />
-                
-                <input
-                    required
-                    type='number'
-                    placeholder='Enter Your Phone Number'
-                    name='phone'
-                    // onChange={handleChange}
-                    // value={userData.phone}
-                />
-
-                <textarea 
-                required
-                type='text'
-                placeholder='Enter Your Message'
-                name='message'
-                />
+                    placeholder='Enter Your Message'
+                    name='message'
+                    onChange={handleChange}
+                    value={formData.message}
+                    />
                
-                <input type = 'submit' value = 'Submit' />
+                    <input type = 'submit' value = 'Submit' />
 
             </form>
 
